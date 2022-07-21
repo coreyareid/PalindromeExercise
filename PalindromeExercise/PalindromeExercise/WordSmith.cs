31cb1abfe292962c72960541abc8353c46dd97c8
coreyareid <coreyareid@bellsouth.net>
2022-07-20 21:20:16 -0500

    Completed Exercise 2

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public WordSmith()
+        {
+        }
+
+        public bool IsPalindrome(string word)
+        {
+            var reverse = "";
+
+            for (int i = word.Length -1; i >= 0; i--)
+            {
+                reverse += word.ToLower()[i];
+            }
+            return reverse != word.ToLower() ? false : true;
+        }
+    }
+}
+
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,26 @@
 using System;
 using Xunit;
-
+using PalindromeExercise;
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        [Theory]
+        [InlineData("civic", true)]
+        [InlineData("madam", true)]
+        [InlineData("chicken", false)]
+        [InlineData("refer", true)]
 
+        public void Tester(string word, bool expected)
+        {
+        //Arrange
+        var wordSmith = new WordSmith();
+
+        //act
+        var actual = wordSmith.IsPalindrome(word);
+
+        //assert
+        Assert.Equal(expected, actual);
         }
     }
 }
